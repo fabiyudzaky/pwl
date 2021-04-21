fc77ba6134624d2933098a7e0d74f848e1d1ee6b
Fauzan Abiyyu Dzaky <fabiyudzaky@yahoo.co.id>
2021-04-21 09:24:34 +0700

    upload praktikum 4 array

---
+++ minggu 4/praktikum/array10.php
@@ -0,0 +1,9 @@
+<?php
+    $arrayBuah = array("Mangga", "Apel", "Pisang", "Kedondong", "Jeruk");
+    if(in_array("Kedondong", $arrayBuah)){
+        echo "Ada buah Kedondong di sini";
+    }
+    else{
+        echo "Tidak ada buah Kedondong di sini";
+    }
+?>
